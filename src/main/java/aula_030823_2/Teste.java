4a7524500c7d8826b0ae92bd88d2c90ee6ddde55
felipe-laudano <felipelaudano@hotmail.com>
2023-08-04 09:43:58 -0300

    Aula 03/08/2023 2 pasta

---
+++ src/main/java/aula_030823_2/Teste.java
@@ -0,0 +1,11 @@
+package aula_030823_2;
+
+import aula_030823.Cliente;
+
+public class Teste {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        //cliente.nome = "Felipe"; var n acessível, nome c/ modif default, fora do pac
+        cliente.cpf = "408.341.569.98"; //var acessível, c/ modif public, mesmo fora do pac
+    }
+}
